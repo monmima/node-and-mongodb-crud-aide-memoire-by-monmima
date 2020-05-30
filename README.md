5c37ab6883b0bed4deadf4d2c7ac9da54b1715c1
monmima <monmima@protonmail.com>
2020-05-30 15:11:56 -0400

    initial commit

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 This is a minimalistic yet quite complete example of a CRUD app with Node.js and MongoDB.
 
-Doing this exercice helped me get my head around how to have Node.js and MongoDB interact with one another.
+Doing this exercice helped me wrap my head around how to have Node.js and MongoDB interact with one another.
+
+# The starting point
+
+The starting point for this exercise was Programming Mentor's tutorial. The final result is now quite different this.
+
+Thank you, Programming Mentor!
+
+Link to tutorial: [http://programmingmentor.com/post/save-form-nodejs-mongodb/](http://programmingmentor.com/post/save-form-nodejs-mongodb/)
+
+Link to video: [https://youtu.be/epo-70M8hNo](https://youtu.be/epo-70M8hNo)
---
+++ app.js
@@ -0,0 +1,169 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+const dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// post
+app.post('/post-feedback', (req, res) => {
+    dbConn.then((db) => {
+        delete req.body._id; // for safety reasons
+        db.collection('feedbacks').insertOne(req.body);
+    });    
+    res.send('Data received:\n' + JSON.stringify(req.body));
+});
+
+// view all
+app.get('/view-all', (req, res) => {
+    dbConn.then((db) => {
+        db.collection('feedbacks').find({}).toArray().then((feedbacks) => {
+            res.status(200).json(feedbacks);
+        });
+    });
+});
+
+// view limit 2
+app.get('/view-limit-2', (req, res) => {
+    dbConn.then((db) => {
+        db.collection('feedbacks').find({}).limit(2).toArray().then((feedbacks) => {
+            res.status(200).json(feedbacks);
+        });
+    });
+});
+
+// rename Ariel
+app.get('/rename-name-field', (req, res) => {
+    dbConn.then((db) => {
+        db.collection('feedbacks').update({name: "Ariel"}, {$rename: {name: "firstName"}});
+    });
+    res.send('Done!');
+});
+
+// increment number ariel (first instance of name "Ariel" if many in database)
+app.get('/increment-ariel', (req, res) => {
+    dbConn.then((db) => {
+        db.collection('feedbacks').update({name: "Ariel"}, {$inc: {number: 2}});
+    });
+    res.send('Done!');
+});
+
+// increment number ariel (first instance of name "Ariel" if many in database)
+app.get('/rename-field-value', (req, res) => {
+    dbConn.then((db) => {
+        db.collection('feedbacks').update({name: "Ariel"}, {$set: {name: "Juliet"}});
+    });
+    res.send('Done!');
+});
+
+// delete the whole collection
+app.get("/delete", (req, res) => {
+
+    dbConn.then((db) => {
+        db.collection('feedbacks').remove();
+    });
+
+    res.send('Done!');
+});
+
+
+// string is exactly "test" in "comment" field
+app.get('/view-string-exactly-test', (req, res) => {
+    dbConn.then((db) => {
+        db.collection('feedbacks').find({comment: "test"}).toArray().then((feedbacks) => {
+            res.status(200).json(feedbacks);
+        });
+    });
+});
+
+// string contains for "test" in "name" or "comment" field
+app.get('/view-string-contains-test', (req, res) => {
+    dbConn.then((db) => {
+
+        db.collection('feedbacks').createIndex({comment: "text", name: "text" });
+
+        db.collection('feedbacks').find( {$text:{$search: "test"}}).toArray().then((feedbacks) => {
+
+            res.status(200).json(feedbacks);
+
+        });
+
+    });
+});
+
+// view values greater than 5
+app.get('/view-greater-than-5', (req, res) => {
+    dbConn.then((db) => {
+        // db.collection('feedbacks').find({number: {$gt: 5}}).toArray().then((feedbacks) => { // >
+        // db.collection('feedbacks').find({number: {$gte: 5}}).toArray().then((feedbacks) => { // >=
+        // db.collection('feedbacks').find({number: {$lt: 5}}).toArray().then((feedbacks) => { // <
+        // db.collection('feedbacks').find({number: {$lte: 5}}).toArray().then((feedbacks) => { // <=
+
+        db.collection('feedbacks').find({number: {$gt: 5}}).toArray().then((feedbacks) => {
+            res.status(200).json(feedbacks);
+        });
+    });
+});
+
+// insert()
+app.get('/insert-one', (req, res) => {
+    dbConn.then((db) => {
+    // console.log("test");
+
+        db.collection('feedbacks').insertMany([
+            {
+                name: "Bob",
+                email: "bob@email.com",
+                number: 7,
+                comment: "test",
+                user: {
+                    added: "automatically",
+                    date: Date()
+                }
+            }
+        ]);
+    });
+
+    res.send('Done!');
+});
+
+// insertMany()
+app.get('/insert-many', (req, res) => {
+    dbConn.then((db) => {
+    // console.log("test");
+
+        db.collection('feedbacks').insertMany([
+            {
+                name: "Jack",
+                email: "jack@email.com",
+                number: 4,
+                comment: "test",
+                user: {
+                    added: "automatically",
+                    date: Date()
+                }
+            },
+            {
+                name: "Ariel",
+                email: "ariel@email.com",
+                number: 2,
+                comment: "test or not?",
+                user: {
+                    added: "automatically",
+                    date: Date()
+                }
+            }
+        ]);
+
+    });
+    res.send('Done!');
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', () => console.log(`Server running on 3000 (most likely)`));
+
+// server.listen(PORT, () => console.log(`Server running on ${PORT}`));

a63a1dc7438e0709d3daeae12167bd8dabe8c7f1
monmima <monmima@protonmail.com>
2020-05-31 18:16:44 -0400

    name of variable improved, app.listen improved as well

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
+const port = process.env.port || 3000;
 
-const dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
+const urlString = mongodb.MongoClient.connect('mongodb://localhost:27017');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 
 // post
 app.post('/post-feedback', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         delete req.body._id; // for safety reasons
         db.collection('feedbacks').insertOne(req.body);
     });    
@@ -21,7 +22,7 @@ app.post('/post-feedback', (req, res) => {
 
 // view EJS template
 app.get('/view-ejs', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').find({}).toArray().then((feedbacks) => {
             res.render("views/template");
         });
@@ -38,7 +39,7 @@ app.set("view engine", "ejs");
 
 // view all ejs
 app.get('/template', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').find({}).toArray().then((feedbacks) => {
             res.render("template", { feedbacks : feedbacks });
 
@@ -49,7 +50,7 @@ app.get('/template', (req, res) => {
 
 // view all
 app.get('/view-all', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').find({}).toArray().then((feedbacks) => {
             res.status(200).json(feedbacks);
         });
@@ -58,7 +59,7 @@ app.get('/view-all', (req, res) => {
 
 // view limit 2
 app.get('/view-limit-2', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').find({}).limit(2).toArray().then((feedbacks) => {
             res.status(200).json(feedbacks);
         });
@@ -67,7 +68,7 @@ app.get('/view-limit-2', (req, res) => {
 
 // rename Ariel
 app.get('/rename-name-field', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').update({name: "Ariel"}, {$rename: {name: "firstName"}});
     });
     res.send('Done!');
@@ -75,7 +76,7 @@ app.get('/rename-name-field', (req, res) => {
 
 // increment number ariel (first instance of name "Ariel" if many in database)
 app.get('/increment-ariel', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').update({name: "Ariel"}, {$inc: {number: 2}});
     });
     res.send('Done!');
@@ -83,7 +84,7 @@ app.get('/increment-ariel', (req, res) => {
 
 // increment number ariel (first instance of name "Ariel" if many in database)
 app.get('/rename-field-value', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').update({name: "Ariel"}, {$set: {name: "Juliet"}});
     });
     res.send('Done!');
@@ -92,7 +93,7 @@ app.get('/rename-field-value', (req, res) => {
 // delete the whole collection
 app.get("/delete", (req, res) => {
 
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').remove();
     });
 
@@ -102,7 +103,7 @@ app.get("/delete", (req, res) => {
 
 // string is exactly "test" in "comment" field
 app.get('/view-string-exactly-test', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         db.collection('feedbacks').find({comment: "test"}).toArray().then((feedbacks) => {
             res.status(200).json(feedbacks);
         });
@@ -111,7 +112,7 @@ app.get('/view-string-exactly-test', (req, res) => {
 
 // string contains for "test" in "name" or "comment" field
 app.get('/view-string-contains-test', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
 
         db.collection('feedbacks').createIndex({comment: "text", name: "text" });
 
@@ -126,7 +127,7 @@ app.get('/view-string-contains-test', (req, res) => {
 
 // view values greater than 5
 app.get('/view-greater-than-5', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
         // db.collection('feedbacks').find({number: {$gt: 5}}).toArray().then((feedbacks) => { // >
         // db.collection('feedbacks').find({number: {$gte: 5}}).toArray().then((feedbacks) => { // >=
         // db.collection('feedbacks').find({number: {$lt: 5}}).toArray().then((feedbacks) => { // <
@@ -140,7 +141,7 @@ app.get('/view-greater-than-5', (req, res) => {
 
 // insert()
 app.get('/insert-one', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
     // console.log("test");
 
         db.collection('feedbacks').insertMany([
@@ -162,7 +163,7 @@ app.get('/insert-one', (req, res) => {
 
 // insertMany()
 app.get('/insert-many', (req, res) => {
-    dbConn.then((db) => {
+    urlString.then((db) => {
     // console.log("test");
 
         db.collection('feedbacks').insertMany([
@@ -200,6 +201,14 @@ app.use(function (req, res, next) {
     res.status(404).send("404 - Sorry can't find that!")
 })
 
-app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', () => console.log(`Server running on 3000 (most likely)`));
-
-// server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+/**
+ * Listening to a port
+ * Steve Griffith
+ * https://www.youtube.com/watch?v=iM_S4RczozU
+ */
+app.listen(port, err => {
+    if (err) {
+        return console.log("ERROR", err);
+    }
+    console.log(`Listening on port ${port}`);
+});
